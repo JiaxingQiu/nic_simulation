8732337c51e21ba269736717e977cc760001a60f
JiaxingQiu <jiaxingq1996@gmail.com>
2024-04-30 09:00:58 -0400

    iid condition simpler

--- sim_conditions_iid.R
+++ sim_conditions_iid.R
@@ -1,14 +1,14 @@
 
 
 # Parameters
-n_cluster <- c(50) #c(50, 100) # number of clusters
-n_obs_per_cluster <- rev(c(5, 10, 50, 100, 150)) # number of observations per cluster
-n_ttl_betas <- seq(5, 10) # number of total effects
-fix_rdm_ratio <- c(0.2) # c(0.2, 0.5, 0.8) # proportion of fix effects
+n_cluster <- 50 #c(50, 100) # number of clusters
+n_obs_per_cluster <- 150 # number of observations per cluster
+n_ttl_betas <- sea(5,10) # number of total effects
+fix_rdm_ratio <- c(1) # c(0.2, 0.5, 0.8) # proportion of fix effects
 sigma_fix <- c(5) # fix effect beta variance # 5
-sigma_rdm_fix_ratio <- rev(c(0))  # random effect beta variance proportional to fixed effects
-ar1_phi <- rev(c(0, 0.4, 0.8)) # runif min, max = min + 0.2, 0-0.2 means low within-cluster correlation, 0.4-0.6 median correlation, 0.8-1 high correlation
-na_rate <- c(0, 1) # 0.3, 0.7,  cluster wise missingness, rnorm centers, 0.3 means low level missingness, 0.7 means high level missingness, 0 means no missing
+sigma_rdm_fix_ratio <- 0  # random effect beta variance proportional to fixed effects
+ar1_phi <- 0 # runif min, max = min + 0.2, 0-0.2 means low within-cluster correlation, 0.4-0.6 median correlation, 0.8-1 high correlation
+na_rate <- 1 # 0.3, 0.7,  cluster wise missingness, rnorm centers, 0.3 means low level missingness, 0.7 means high level missingness, 0 means no missing
 # 1 means unbalanced missingness, ranging from 0-1
 
 # Define the simulation conditions
